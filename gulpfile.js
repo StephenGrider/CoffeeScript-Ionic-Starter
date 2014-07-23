ae37ff350fc7cdc7149535e9ada72414e5e37773
StephenGrider <ste.grider@gmail.com>
2014-07-22 18:42:00 -0700

    gulpfile sass, coffee, watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,45 +6,34 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var coffee = require('gulp-coffee');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./www/**/*.scss'],
+  coffee: ['./www/**/*.coffee']
+
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+  gulp.src('./www/sass/**.scss')
+    .pipe(sass({ errLogToConsole: true}))
+    .pipe(rename({ extname: '.css' }))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./www/css'))
     .on('end', done);
 });
 
+gulp.task('coffee', function(done) {
+  gulp.src(paths.coffee)
+  .pipe(coffee({bare: true}).on('error', gutil.log))
+  .pipe(concat('application.js'))
+  .pipe(gulp.dest('./www/js'))
+  .on('end', done)
+})
+
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+  gulp.watch(paths.sass, ['sass'])
+  gulp.watch(paths.coffee, ['coffee'])
 });
